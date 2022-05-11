ff58cedc2d324ebdb8734109415fa9b9bbd10344
V.T.TuongDi <vothituongduy20@gmail.com>
2022-05-11 10:46:20 +0700

    First commit

---
+++ src/main/java/com/teethcare/TeethCareApplication.java
@@ -0,0 +1,13 @@
+package com.teethcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeethCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeethCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teethcare/TeethCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teethcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeethCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
