c6505154b85324b2e7c59ebe17561a1fa420f0ff
Nguyễn Trung Tín <86908613+trungtin1811@users.noreply.github.com>
2022-05-19 00:43:38 +0700

    Revert "Git checkout  b teet 15 as a patient i want to view the information of selected dental center"

--- src/main/java/com/teethcare/TeethCareApplication.java
+++ src/main/java/com/teethcare/TeethCareApplication.java
@@ -3,23 +3,20 @@ package com.teethcare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableSwagger2
 public class TeethCareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeethCareApplication.class, args);
     }
-//
-//    @Bean
-//    PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
