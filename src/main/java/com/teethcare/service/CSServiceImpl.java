4a3ce6c2aee72f61c82f3c19dd92ed2dd20edb8e
Nguyen Thanh Ha <nthoreoo2602@gmail.com>
2022-05-18 23:05:18 +0700

    Implement DentistService

---
+++ src/main/java/com/teethcare/service/CSServiceImpl.java
@@ -0,0 +1,63 @@
+package com.teethcare.service;
+
+import com.teethcare.model.entity.Customer;
+import com.teethcare.model.entity.CustomerService;
+import com.teethcare.repository.CustomerServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements AccountService<CustomerService>{
+
+    private CustomerServiceRepository customerServiceRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerServiceRepository customerServiceRepository) {
+        this.customerServiceRepository = customerServiceRepository;
+    }
+
+    @Override
+    public List<CustomerService> findAll() {
+        return null;
+    }
+
+    @Override
+    public CustomerService findById(int theId) {
+        return null;
+    }
+
+    @Override
+    public CustomerService findById(String theId) {
+        Optional<CustomerService> result = customerServiceRepository.findById(theId);
+
+        CustomerService theCustomerService = null;
+
+        if (result.isPresent()) {
+            theCustomerService = result.get();
+        }
+        else {
+            // we didn't find the employee
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+
+        return theCustomerService;
+    }
+
+    @Override
+    public void save(CustomerService theCustomerService) {
+        customerServiceRepository.save(theCustomerService);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+    }
+
+    @Override
+    public void deleteById(String theId) {
+        customerServiceRepository.deleteById(theId);
+    }
+}
--- src/main/java/com/teethcare/service/DentistServiceImpl.java
+++ src/main/java/com/teethcare/service/DentistServiceImpl.java
@@ -1,2 +1,64 @@
-package com.teethcare.service;public class DentistServiceImpl {
+package com.teethcare.service;
+
+import com.teethcare.model.entity.CustomerService;
+import com.teethcare.model.entity.Dentist;
+import com.teethcare.repository.DentistRepository;
+import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DentistServiceImpl implements AccountService<Dentist> {
+    private DentistRepository dentistRepository;
+
+    @Autowired
+    public DentistServiceImpl(DentistRepository dentistRepository) {
+        this.dentistRepository = dentistRepository;
+    }
+
+    @Override
+    public Dentist findById(String theId) {
+        Optional<Dentist> result = dentistRepository.findById(theId);
+
+        Dentist theDentist = null;
+
+        if (result.isPresent()) {
+            theDentist = result.get();
+        }
+        else {
+            // we didn't find the employee
+            throw new RuntimeException("Did not find employee id - " + theId);
+        }
+
+        return theDentist;
+    }
+
+    @Override
+    public void deleteById(String theId) {
+        dentistRepository.deleteById(theId);
+    }
+
+    @Override
+    public List<Dentist> findAll() {
+        return null;
+    }
+
+    @Override
+    public Dentist findById(int theId) {
+        return null;
+    }
+
+    @Override
+    public void save(Dentist theDentist) {
+        dentistRepository.save(theDentist);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+    }
 }
