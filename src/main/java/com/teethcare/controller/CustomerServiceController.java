abac72510163fa6e1155c3726968bf9450a06d79
trungtin1811 <86908613+trungtin1811@users.noreply.github.com>
2022-05-19 00:35:20 +0700

    CRUD APPLICATION_OWNER + MANAGER + CUSTOMER + CUSTOMER_SERVICE + CLINIC

---
+++ src/main/java/com/teethcare/controller/CustomerServiceController.java
@@ -0,0 +1,46 @@
+package com.teethcare.controller;
+
+import com.teethcare.model.entity.CustomerService;
+import com.teethcare.repository.CustomerServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@EnableSwagger2
+@RequestMapping("/api/customer-services")
+public class CustomerServiceController {
+    @Autowired
+    private CustomerServiceRepository customerServiceRepository;
+
+    @GetMapping
+    public List<CustomerService> getAllCustomerServices() {
+        return customerServiceRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<CustomerService> getCustomerService(@PathVariable("id") String id) {
+        return customerServiceRepository.findById(id);
+    }
+
+    @PostMapping
+    public CustomerService addCustomerService(@RequestBody CustomerService customerService) {
+        return customerServiceRepository.save(customerService);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerService> delCustomerService(@PathVariable("id") String id) {
+        Optional<CustomerService> customerServiceData = customerServiceRepository.findById(id);
+        if (customerServiceData.isPresent()) {
+            CustomerService customerService = customerServiceData.get();
+            customerService.getAccount().setStatus(false);
+            return new ResponseEntity<>(customerServiceRepository.save(customerService), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/teethcare/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.teethcare.repository;
+
+import com.teethcare.model.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+}
