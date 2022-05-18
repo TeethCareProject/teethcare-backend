e6a8fc4e1c26e9d56bf83d551d781b7904353dc6
Nguyen Thanh Ha <nthoreoo2602@gmail.com>
2022-05-18 23:06:38 +0700

    Implement CSService

--- src/main/java/com/teethcare/service/CSServiceImpl.java
+++ src/main/java/com/teethcare/service/CSServiceImpl.java
@@ -1,6 +1,5 @@
 package com.teethcare.service;
 
-import com.teethcare.model.entity.Customer;
 import com.teethcare.model.entity.CustomerService;
 import com.teethcare.repository.CustomerServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +9,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CustomerServiceImpl implements AccountService<CustomerService>{
+public class CSServiceImpl implements AccountService<CustomerService>{
 
     private CustomerServiceRepository customerServiceRepository;
 
     @Autowired
-    public CustomerServiceImpl(CustomerServiceRepository customerServiceRepository) {
+    public CSServiceImpl(CustomerServiceRepository customerServiceRepository) {
         this.customerServiceRepository = customerServiceRepository;
     }
 
