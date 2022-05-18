3f871eaa187181ed6cb1c35b6a6611d990c2febf
Nguyen Thanh Ha <nthoreoo2602@gmail.com>
2022-05-18 23:01:50 +0700

    Extend interface CRUDService

--- src/main/java/com/teethcare/service/AccountService.java
+++ src/main/java/com/teethcare/service/AccountService.java
@@ -1,4 +1,7 @@
 package com.teethcare.service;
 
-public interface AccountService {
+public interface AccountService<T> extends CRUDService<T>{
+    T findById(String theId);
+
+    void deleteById(String theId);
 }
