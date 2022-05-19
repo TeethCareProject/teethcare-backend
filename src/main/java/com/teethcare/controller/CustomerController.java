eba3520e53618cc1feab7a76ad7e318bef507bfa
trungtin1811 <86908613+trungtin1811@users.noreply.github.com>
2022-05-19 17:04:09 +0700

    using service

--- src/main/java/com/teethcare/controller/CustomerController.java
+++ src/main/java/com/teethcare/controller/CustomerController.java
@@ -1,10 +1,8 @@
 package com.teethcare.controller;
 
 import com.teethcare.model.entity.Customer;
-import com.teethcare.model.entity.Customer;
-import com.teethcare.repository.CustomerRepository;
+import com.teethcare.service.CustomerServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,31 +15,25 @@ import java.util.Optional;
 @RequestMapping("/api/customers")
 public class CustomerController {
     @Autowired
-    private CustomerRepository customerRepository;
+    private CustomerServiceImp customerServiceImp;
 
     @GetMapping
     public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
+        return customerServiceImp.findAll();
     }
 
     @GetMapping("/{id}")
-    public Optional<Customer> getCustomer(@PathVariable("id") String id) {
-        return customerRepository.findById(id);
+    public Optional<Customer> getCustomer(@PathVariable("id") Integer id) {
+        return customerServiceImp.findById(id);
     }
 
     @PostMapping
     public Customer addCustomer(@RequestBody Customer customer) {
-        return customerRepository.save(customer);
+        return customerServiceImp.save(customer);
     }
 
-    @PutMapping ("/{id}")
-    public ResponseEntity<Customer> delCustomer(@PathVariable("id") String id) {
-        Optional<Customer> CustomerData = customerRepository.findById(id);
-        if (CustomerData.isPresent()) {
-            Customer customer = CustomerData.get();
-            customer.setStatus(false);
-            return new ResponseEntity<>(customerRepository.save(customer), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<Customer> delCustomer(@PathVariable("id") Integer id) {
+        return customerServiceImp.delete(id);
     }
 }
---
+++ src/main/java/com/teethcare/service/CustomerServiceImp.java
@@ -0,0 +1,42 @@
+package com.teethcare.service;
+
+import com.teethcare.model.entity.Customer;
+import com.teethcare.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImp implements CRUDService<Customer> {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(Integer id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+         return  customerRepository.save(customer);
+    }
+    @Override
+    public ResponseEntity delete(Integer id) {
+        Optional<Customer> CustomerData = customerRepository.findById(id);
+        if (CustomerData.isPresent()) {
+            Customer customer = CustomerData.get();
+            customer.setStatus(false);
+            return new ResponseEntity<>(customerRepository.save(customer), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
