0e850034814e52ab9a1d6371dbe1c0f7fd26bdfc
V.T.TuongDi <vothituongduy20@gmail.com>
2022-06-04 10:33:24 +0700

    Update create booking

--- src/main/java/com/teethcare/model/response/PatientBookingResponse.java
+++ src/main/java/com/teethcare/model/response/PatientBookingResponse.java
@@ -1,9 +1,12 @@
 package com.teethcare.model.response;
 
+import com.teethcare.model.entity.ServiceOfClinic;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,4 +21,5 @@ public class PatientBookingResponse {
     private String serviceName;
     private String description;
     private long desiredCheckingTime;
+    private ClinicSimpleResponse clinic;
 }
