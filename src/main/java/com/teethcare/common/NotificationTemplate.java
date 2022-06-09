18e96e0180680b4dfc9bc2f857131ad462ebe9bf
Nesto <se.nttin1811@gmail.com>
2022-06-09 20:55:01 +0700

    fix message

--- src/main/java/com/teethcare/common/NotificationTemplate.java
+++ src/main/java/com/teethcare/common/NotificationTemplate.java
@@ -3,5 +3,5 @@ package com.teethcare.common;
 import com.teethcare.model.request.NotificationMsgRequest;
 
 public class NotificationTemplate {
-    public static NotificationMsgRequest CHECK_IN_NOTIFICATION = new NotificationMsgRequest(null, "Check in success", "Check in success", "/", null, NotificationType.OPEN_BOOKING_NOTIFICATION.name());
+    public static NotificationMsgRequest CHECK_IN_NOTIFICATION = new NotificationMsgRequest(null, "Open Booking Detail", "Click to open the booking detail!", "/", null, NotificationType.OPEN_BOOKING_NOTIFICATION.name());
 }
