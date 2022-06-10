0075c9b7a77ab4b702a0385ecd9e94287b76d9b6
trungtin1811 <se.nttin1811@gmail.com>
2022-06-10 16:01:33 +0700

    fix FirebaseConfig

--- src/main/java/com/teethcare/config/firebase/FirebaseConfig.java
+++ src/main/java/com/teethcare/config/firebase/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,10 +13,13 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${firebase.service-account}")
+    private String serviceAccount;
+
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource("service-account.json").getInputStream());
+                .fromStream(new ClassPathResource(serviceAccount).getInputStream());
         FirebaseOptions firebaseOptions = FirebaseOptions
                 .builder()
                 .setCredentials(googleCredentials)
