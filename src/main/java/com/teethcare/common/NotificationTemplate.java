d89874735cee8d56e87501239be8d15151d0d0a0
Nguyen Thanh Ha <nthoreoo2602@gmail.com>
2022-06-11 20:41:53 +0700

    Update notification template for update booking

--- src/main/java/com/teethcare/common/NotificationTemplate.java
+++ src/main/java/com/teethcare/common/NotificationTemplate.java
@@ -4,4 +4,8 @@ import com.teethcare.model.request.NotificationMsgRequest;
 
 public class NotificationTemplate {
     public static NotificationMsgRequest CHECK_IN_NOTIFICATION = new NotificationMsgRequest(null, NotificationType.OPEN_BOOKING_NOTIFICATION.name(), null, "/", null, NotificationType.OPEN_BOOKING_NOTIFICATION.name());
+    public static NotificationMsgRequest UPDATE_BOOKING_1ST = new NotificationMsgRequest(null, NotificationType.UPDATE_BOOKING_1ST.name(),
+            null, "/", null, NotificationType.UPDATE_BOOKING_1ST.name());
+    public static NotificationMsgRequest UPDATE_BOOKING_2RD = new NotificationMsgRequest(null, NotificationType.UPDATE_BOOKING_2RD.name(),
+            null, "/", null, NotificationType.UPDATE_BOOKING_2RD.name());
 }
--- src/main/java/com/teethcare/common/NotificationType.java
+++ src/main/java/com/teethcare/common/NotificationType.java
@@ -2,5 +2,7 @@ package com.teethcare.common;
 
 public enum NotificationType {
     NORMAL_NOTIFICATION,
-    OPEN_BOOKING_NOTIFICATION
+    OPEN_BOOKING_NOTIFICATION,
+    UPDATE_BOOKING_1ST,
+    UPDATE_BOOKING_2RD
 }
