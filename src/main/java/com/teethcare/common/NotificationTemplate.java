5effe57ed6e8f841b8ef181f644bc517e5ab7322
Nguyen Thanh Ha <nthoreoo2602@gmail.com>
2022-06-14 15:54:54 +0700

    Change forward link

--- src/main/java/com/teethcare/common/NotificationTemplate.java
+++ src/main/java/com/teethcare/common/NotificationTemplate.java
@@ -4,9 +4,4 @@ import com.teethcare.model.request.NotificationMsgRequest;
 
 public class NotificationTemplate {
     public static NotificationMsgRequest CHECK_IN_NOTIFICATION = new NotificationMsgRequest(null, NotificationType.OPEN_BOOKING_NOTIFICATION.name(), null, "/", null, NotificationType.OPEN_BOOKING_NOTIFICATION.name());
-    public static NotificationMsgRequest UPDATE_BOOKING_1ST = new NotificationMsgRequest(null, NotificationType.UPDATE_BOOKING_1ST_NOTIFICATION.name(),
-            null, "/", null, NotificationType.UPDATE_BOOKING_1ST_NOTIFICATION.name());
-    public static NotificationMsgRequest UPDATE_BOOKING_2RD = new NotificationMsgRequest(null,
-            NotificationType.UPDATE_BOOKING_2RD_NOTIFICATION.name(),
-            null, "/", null, NotificationType.UPDATE_BOOKING_2RD_NOTIFICATION.name());
 }
