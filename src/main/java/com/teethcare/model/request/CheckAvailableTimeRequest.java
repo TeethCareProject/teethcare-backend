ff6281dd994c6d1c211f6714114a9fac7ead4ea3
trungtin1811 <se.nttin1811@gmail.com>
2022-06-21 22:19:58 +0700

    check available

--- src/main/java/com/teethcare/model/request/CheckAvailableTimeRequest.java
+++ src/main/java/com/teethcare/model/request/CheckAvailableTimeRequest.java
@@ -3,9 +3,13 @@ package com.teethcare.model.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class CheckAvailableTimeRequest {
+    @NotNull
     Integer clinicId;
+    @NotNull
     Long desiredCheckingTime;
 }
