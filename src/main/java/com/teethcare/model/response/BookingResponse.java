04469c67fe50d88d61e68f5c3f01619144d4a03b
ZERO-94 <kimanh.tvka@gmail.com>
2022-06-27 00:46:33 +0700

    fix clinic simple response

--- src/main/java/com/teethcare/model/response/BookingResponse.java
+++ src/main/java/com/teethcare/model/response/BookingResponse.java
@@ -30,7 +30,7 @@ public class BookingResponse {
     private boolean isConfirmed;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<ServiceOfClinicResponse> services;
-    private ClinicInfoResponse clinic;
+    private ClinicSimpleResponse clinic;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private FeedbackResponse feedbackResponse;
 }
